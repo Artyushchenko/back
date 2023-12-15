79ec120e204f940ea283172f89aaf70392d1f3e0
Artyushchenko <Artyushchenko2018@gmail.com>
2023-12-15 22:53:02 +0200

    Guards and decorators were created

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,6 +8,7 @@ import {
 } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthDto } from './dto/auth.dto'
+import { RefreshTokenDto } from './dto/refreshToken.dto'
 
 @Controller('auth')
 export class AuthController {
@@ -15,9 +16,16 @@ export class AuthController {
 
 	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
-	@Post('login')
+	@Post('login/access-token')
 	async login(@Body() dto: AuthDto) {
 		return this.AuthService.login(dto)
+	}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('login')
+	async getNewTokens(@Body() dto: RefreshTokenDto) {
+		return this.AuthService.getNewTokens(dto)
 	}
 
 	@UsePipes(new ValidationPipe())
